ab3fefdba0c4a368fc26a740d2034b62496c72af
AK <ashokvardhanreddy96@gmail.com>
2021-12-17 12:50:52 -0500

    Update Deployment.groovy

--- Deployment.groovy
+++ Deployment.groovy
@@ -3,11 +3,10 @@ pipeline {
     stages {
         stage('echo parameters') {
             steps {
-                echo "DEBUG: parameter artifactId = ${env.artifactId}"
-                echo "DEBUG: parameter appVersion = ${env.appVersion}"
-                echo "DEBUG: parameter IdClusterNamespaceApi = ${IdClusterNamespaceApi}"
-                echo "DEBUG: parameter ClusterLocation = ${clusterLocation}"
-                echo "DEBUG: parameter api_01_appVersion = ${appVersion}"
+                echo "DEBUG: parameter projectIdClusterNamespaceApiProxy = ${projectIdClusterNamespaceApiProxy}"
+                echo "DEBUG: parameter clusterLocation = ${clusterLocation}"
+                echo "DEBUG: parameter artifactId = ${artifactId}"
+                echo "DEBUG: parameter appVersion = ${appVersion}"
             }
         }
     }
